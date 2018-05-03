c347518275b15122cae502c942284f358d797d82
lixufei <xfli@thoughtworks.com>
2018-05-03 12:19:03 +0800

    remove useless images for maxmun size of upload size

--- pages/now/index.js
+++ pages/now/index.js
@@ -10,8 +10,11 @@ Page({
         navigationImage: '/image/swipe-1.jpg',
         discriptionTitle: '1手镯的世界',
         descriptionMedia: {
-          images: [],
-          video: '/image/ads.mp4',
+          images: [
+            '/image/home-now-cover.jpg',
+            '/image/home-table-cover.jpg',
+            '/image/watch-cover.jpg',
+          ]
         },
         descriptionContent: '1Silver 925 rose gold plated flexible cuff bracelet with clear cubic zirconia stones from the My FF collection. Do it like a fashionista and wear as many as possible in one wrist for the ultimate result.',
         more: [
