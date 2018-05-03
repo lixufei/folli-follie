77b2e2fc70947d9e0e2ab193d22262661933a550
lixufei <xfli@thoughtworks.com>
2018-05-03 18:02:39 +0800

    add image navigation component

--- pages/index/index.js
+++ pages/index/index.js
@@ -182,6 +182,11 @@ Page({
       url: '../store/index',
     })
   },
+  handleMyFF: function() {
+    wx.navigateTo({
+      url: '../myFF/index',
+    })
+  },    
 
   /**
    * 生命周期函数--监听页面初次渲染完成
